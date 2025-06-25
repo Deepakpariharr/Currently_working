55daff324d4ea6b063f65beb53eff1c7407b6bfc
deepak <deepak2001parihar@gmail.com>
2025-06-25 13:24:06 +0530

    new_file

---
+++ customer_analytics.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+import plotly.express as px
+import plotly.graph_objects as go
+from datetime import datetime, timedelta
+
