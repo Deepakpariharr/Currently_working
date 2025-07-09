7378e76f5692aa8f481c5269f91197278cacfa28
deepak <deepak2001parihar@gmail.com>
2025-07-09 22:52:29 +0530

    start

--- customer_analytics.py
+++ customer_analytics.py
@@ -6,3 +6,20 @@ import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
 
+class OlistCustomerAnalytics:
+    def __init__(self, db_connection):
+        self.db = db_connection
+
+    def brazilian_rfm_analysis(self):
+        """
+        RFM analysis specifically tailored for Brazillian e-commerec patterns
+        """
+
+        query = """
+SELECT 
+     c.customer_id,
+     c.customer_state,
+     c.customer_city,
+     MAX(o.order_purchase_timestamp) as last_purchase,
+     COUNT(DITINCT o.order_id) as frequency,
+     """
